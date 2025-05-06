1001d8bc2634a2b061bc1c5df1e62c031bbd826f
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-05-06 13:25:12 +0530

    Add display module with initialization and rendering functions

--- code/code.ino
+++ code/code.ino
@@ -1,10 +1,14 @@
 #include "webserve.h"
 #include "controllerEndpoints.h"
+#include "display.h"
 #include <Arduino.h>
+
 
 void setup() {
     Serial.begin(115200);
     setupWeb();
+    setupDisplay();
+    Serial.println("Setup complete");
 }
 
 
