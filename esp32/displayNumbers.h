96d34625237a202b52be452f68c5e9aa53193121
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-12-07 15:04:39 +0530

    deleted repeatitive files

--- esp32/displayNumbers.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <FastLED.h>
-
-void drawCenteredTwoDigitNumber(int value, CRGB color = CRGB::Blue);
-void drawDigit(int digit, int startX, int startY, CRGB color);
--- esp32/snake.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void setupSnakeGame();
-void gameTick();
-void runSnakeGame();
-void spawnFood();
